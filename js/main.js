636461e61e8054f3a3bc00a58c02650da8242562
Wlodek Bzyl <matwb@univ.gda.pl>
2013-11-12 15:34:14 +0100

    poprawki

--- js/main.js
+++ js/main.js
@@ -4,30 +4,36 @@
     console.log('is CORS supported?', $.support.cors);
     console.log('tweets count: ', data.count);
   });
-       
+
   var q = {
     sort:  { created_at: { order: "desc" } },
     from: 0,
-    size: 2
+    size: 4
   };
+       
+  $('#ajaxform').submit(function(event) {
+    q.query = { query_string: { query: $('#ajaxform input').val() } }; 
+    var query = JSON.stringify(q);
+    console.log('elasticsearch query: ', query)
 
-  q.query = { query_string: { query: "redis" } }; 
+    $.ajax({
+      // url: "/tweets/_search",
+      url: "http://localhost:9200/tweets/_search",
+      type: "POST",
+      data : query
+    })
+      .done(function( data ) {
+        console.log('total: ', data.hits.total);
+        data.hits.hits.forEach(function(x) {
+          console.log(JSON.stringify(x._source));
+        });
+      })
+      .fail(function( jqxhr, textStatus, error ) {
+        var err = textStatus + ", " + error;
+        console.log( "Request Failed: " + err );
+      });
 
-  $.ajax({
-    // url: "/tweets/_search",
-    url: "http://localhost:9200/tweets/_search",
-    type: "POST",
-    data : JSON.stringify(q)
-  })
-  .done(function( data ) {
-    console.log('total: ', data.hits.total);
-    data.hits.hits.forEach(function(x) {
-      console.log(JSON.stringify(x._source));
-    });
-  })
-  .fail(function( jqxhr, textStatus, error ) {
-    var err = textStatus + ", " + error;
-    console.log( "Request Failed: " + err );
+    event.preventDefault();
   });
 
 })();
